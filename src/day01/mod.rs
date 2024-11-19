5d7ae59ff7a3c834b789a384addef93d8a2c8966
Are Velde Jensen <are.velde.jensen@nrk.no>
2024-11-19 15:51:46 +0100

    Tweaking default day setup

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -3,20 +3,26 @@ use crate::utils::solution::Solution;
 use anyhow::anyhow;
 use anyhow::Result;
 
+const DAY_STR: &str = "1";
+
 pub fn run(part: u8) -> Result<()> {
-    let input = file_reader::read_text_from_file("1");
-    match part {
-        1 => Ok(()),
-        2 => Ok(()),
+    let input = file_reader::read_text_from_file(DAY_STR)?;
+    let answer = match part {
+        1 => part1(&input),
+        2 => part2(&input),
         val => Err(anyhow!("got a part thats not 1 or 2: {}", val)),
-    }
+    }?;
+
+    println!("The answer for part {part} on {DAY_STR} is probably: {answer}");
+
+    Ok(())
 }
 
-fn part1(input: &str) -> Solution {
+fn part1(input: &str) -> Result<Solution> {
     todo!()
 }
 
-fn part2(input: &str) -> Solution {
+fn part2(input: &str) -> Result<Solution> {
     todo!()
 }
 
@@ -30,16 +36,18 @@ mod tests {
     const TEST_INPUT_TWO: &str = r"";
 
     #[test]
-    fn test_part_1() {
+    fn test_part_1() -> Result<()> {
         let fasit = Solution::from(142u32);
-        let part_solution = part1(TEST_INPUT_ONE);
+        let part_solution = part1(TEST_INPUT_ONE)?;
         assert_eq!(fasit, part_solution);
+        Ok(())
     }
 
     #[test]
-    fn test_part_2() {
+    fn test_part_2() -> Result<()> {
         let fasit = Solution::from(281u32);
-        let my_soultion = part2(TEST_INPUT_TWO);
+        let my_soultion = part2(TEST_INPUT_TWO)?;
         assert_eq!(fasit, my_soultion);
+        Ok(())
     }
 }
